e115ace2bbc3b9043b8ea44b16b7fd813638f0bb
yaredow <yaredyilma11@gmail.com>
2024-09-25 08:56:38 +0300

    change the url to the dog route to use the backend docker network

--- dogdb-frontend/src/data/breed.ts
+++ dogdb-frontend/src/data/breed.ts
@@ -1,8 +1,7 @@
 export const getAllbreeds = async () => {
   try {
-    const response = await fetch("http://localhost:5000/api/v1/breed", {
+    const response = await fetch("http://backend:5000/api/v1/breed", {
       method: "GET",
-      cache: "no-cache",
     });
     if (!response.ok) return [];
 
@@ -17,9 +16,8 @@ export const getAllbreeds = async () => {
 
 export const getBreedWithSlug = async (slug: string) => {
   try {
-    const response = await fetch(`http://localhost:5000/api/v1/breed/${slug}`, {
+    const response = await fetch(`http://backend:5000/api/v1/breed/${slug}`, {
       method: "GET",
-      cache: "no-cache",
     });
 
     if (!response.ok) return [];
