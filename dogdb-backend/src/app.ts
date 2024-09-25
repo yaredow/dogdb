944b8821df8618a0e817f85a3e7269a1b4e5847f
yaredow <yaredyilma11@gmail.com>
2024-09-25 13:30:06 +0300

    add the vps ip as origin the backend cors option

--- dogdb-backend/src/app.ts
+++ dogdb-backend/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://198.244.232.203"],
     credentials: true,
   }),
 );
