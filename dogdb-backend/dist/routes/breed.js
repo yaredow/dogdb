103f55ae95b688437812e169e46d71185adec183
yaredow <yaredyilma11@gmail.com>
2024-09-30 10:53:25 +0300

    fix the production backend url

--- dogdb-backend/dist/routes/breed.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const breedController_1 = require("../controllers/breedController");
-const router = (0, express_1.Router)();
-router.get("/", breedController_1.getAllBreeds);
-router.get("/:slug", breedController_1.getBreed);
-exports.default = router;
