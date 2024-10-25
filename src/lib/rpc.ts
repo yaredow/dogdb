4bfced05177675837eab00239c35bd1716c60d04
yaredow <yaredyilma11@gmail.com>
2024-10-25 10:01:14 +0300

    fix the rpc public api url

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,4 +1,4 @@
 import { hc } from "hono/client";
 import { AppType } from "@/app/api/[[...route]]/route";
 
-export const client = hc<AppType>("https://yaredow-dogdb.vercel.app");
+export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
