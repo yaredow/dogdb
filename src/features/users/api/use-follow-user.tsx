295c89c79b5de0a32f6410e989aab0c988792ca7
yaredow <yaredyilma11@gmail.com>
2024-11-08 06:58:56 +0000

    finish user following hook

---
+++ src/features/users/api/use-follow-user.tsx
@@ -0,0 +1,51 @@
+import { InferRequestType, InferResponseType } from "hono";
+
+import { useMutation } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+import { toast } from "@/hooks/use-toast";
+
+type UseFollowUser = {
+  userId: string;
+};
+
+type ResponseType = InferResponseType<
+  (typeof client.api.users)["follow"][":userId"]["$post"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.users)["follow"][":userId"]["$post"]
+>;
+
+export const useFollowUser = ({ userId }: UseFollowUser) => {
+  const { mutate: startConversation, isPending } = useMutation<
+    ResponseType,
+    Error,
+    RequestType
+  >({
+    mutationFn: async () => {
+      const response = await client.api.users["follow"][":userId"].$post({
+        param: {
+          userId,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to following user");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast({
+        description: "Follow successful",
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      toast({
+        description: "Error following user",
+        variant: "destructive",
+      });
+    },
+  });
+  return { startConversation, isPending };
+};
