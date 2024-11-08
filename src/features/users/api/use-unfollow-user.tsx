0cf57a56ab7a2b36932e72e3613ca031d0016572
yaredow <yaredyilma11@gmail.com>
2024-11-08 07:02:07 +0000

    finish user unfollowing hook

---
+++ src/features/users/api/use-unfollow-user.tsx
@@ -0,0 +1,51 @@
+import { InferRequestType, InferResponseType } from "hono";
+
+import { useMutation } from "@tanstack/react-query";
+import { client } from "@/lib/rpc";
+import { toast } from "@/hooks/use-toast";
+
+type UseUnfollowUser = {
+  userId: string;
+};
+
+type ResponseType = InferResponseType<
+  (typeof client.api.users)["unfollow"][":userId"]["$delete"]
+>;
+type RequestType = InferRequestType<
+  (typeof client.api.users)["unfollow"][":userId"]["$delete"]
+>;
+
+export const useUnfollowUser = ({ userId }: UseUnfollowUser) => {
+  const { mutate: startConversation, isPending } = useMutation<
+    ResponseType,
+    Error,
+    RequestType
+  >({
+    mutationFn: async () => {
+      const response = await client.api.users["unfollow"][":userId"].$delete({
+        param: {
+          userId,
+        },
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to unfollow user");
+      }
+
+      return await response.json();
+    },
+    onSuccess: () => {
+      toast({
+        description: "You unfollowed this user",
+      });
+    },
+    onError: (error) => {
+      console.error(error);
+      toast({
+        description: "Error unfollowing this user",
+        variant: "destructive",
+      });
+    },
+  });
+  return { startConversation, isPending };
+};
