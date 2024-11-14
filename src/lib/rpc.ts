6be9522dd122df222c8018541175a6ff3f72d60a
yaredow <yaredyilma11@gmail.com>
2024-11-14 20:09:50 +0000

    add a remote db connection string

--- src/lib/rpc.ts
+++ src/lib/rpc.ts
@@ -1,7 +1,4 @@
 import { hc } from "hono/client";
-import { AppType, WebSocketApp } from "@/app/api/[[...route]]/route";
+import { AppType } from "@/app/api/[[...route]]/route";
 
 export const client = hc<AppType>(process.env.NEXT_PUBLIC_API_URL!);
-export const socket = hc<WebSocketApp>(
-  process.env.NEXT_PUBLIC_API_URL!,
-).ws.$ws();
